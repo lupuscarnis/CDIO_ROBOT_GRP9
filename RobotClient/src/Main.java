d42e9ae0a39854ba9fe10d00d9606e2628b2b7b5
Nicolai Barnett <lupuscarnis@gmail.com>
2019-03-20 09:43:28 +0100

    Added the start to a GUI for the robot

--- RobotClient/src/Main.java
+++ RobotClient/src/Main.java
@@ -5,35 +5,19 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-
+import gui.*;
 public class Main {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 
-	
 		
 		
-		Socket socket = new Socket("192.168.43.69", 4444);
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		if (socket.isConnected() == true) {
-			System.out.println("Connection established");
-
-		} else {
-			System.out.println("Connection Failed!");
-		}
-		Scanner key = new Scanner(System.in);
-		String userInput= "x";
-		
-		 
-		  
-		  
-		out.println(userInput);
-		while (!(userInput.equals("peace"))) {
-			userInput = key.nextLine();
-			out.println(userInput);
-
-		}
-
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GUI_Frame.createAndShowGUI();
+                
+            }
+        });
 	}
+	
 }
--- RobotClient/src/gui/GUI_Frame.java
+++ RobotClient/src/gui/GUI_Frame.java
@@ -1,19 +1,63 @@
 package gui;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
 
-public class GUI_Frame {
-	//1. Create the frame.
-	JFrame frame = new JFrame("FrameDemo");
+/* FrameDemo.java requires no other files. */
+public class GUI_Frame extends JPanel implements ActionListener{
+	static Label s;
+	protected JTextArea textArea;
+	/**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    public static void createAndShowGUI() {
+    	s = new Label("Sensor input");
+    	JButton button = new JButton("Press");
+    	JFrame frame = new JFrame("Robot GUI");
+    	button.setSize(new Dimension(100,100));
+    	s.setSize(new Dimension(200,100));
+    	
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(s);
+        frame.getContentPane().add(button);
+        frame.addKeyListener(new KeyListener() {
+ 
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println(KeyEvent.getKeyText(e.getKeyCode()));
+				
+			}
+		});
+        
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(300, 300));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
-	//2. Optional: What happens when the frame closes?
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
-	//3. Create components and put them in the frame.
-	//...create emptyLabel...
-	frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-	//4. Size the frame.
-	frame.pack();
-
-	//5. Show it.
-	frame.setVisible(true);
 }
